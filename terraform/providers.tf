d955624a141af2f39ef320f5d8e4451713aa6564
Faizan Mansuri <faizanahmedmansuri@gmail.com>
2026-05-06 19:32:00 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,13 @@
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.43.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region = "ap-south-1"
 }
